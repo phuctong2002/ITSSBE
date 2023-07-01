c13155f6d40a0aaee52f159c3fe444d9a6bec9bf
tuan-trankhac <tuan.tk200554@sis.hust.edu.vn>
2023-07-01 13:27:17 +0700

    PushMaiKDuoc

---
+++ src/main/java/com/example/ITSSBE/entity/FeedBack.java
@@ -0,0 +1,28 @@
+package com.example.ITSSBE.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table( name = "feedback")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class FeedBack {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column( name = "id")
+    private int id;
+    @Column( name = "content")
+    private String content;
+    @Column( name = "created_at")
+    private Date created_at;
+    @ManyToOne( fetch = FetchType.EAGER)
+    @JoinColumn( name = "user")
+    private User by_user;
+}
