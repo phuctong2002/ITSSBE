d51df1ac3e78b4a7ceba00dc366c69e75b5fef24
Nguyen Hoang Anh <hoanganh8d@gmail.com>
2023-07-09 09:06:31 +0700

    Add test for getEquipment function in EquipmentService

--- src/test/java/com/example/ITSSBE/ItssbeApplicationTests.java
+++ src/test/java/com/example/ITSSBE/ItssbeApplicationTests.java
@@ -15,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -102,6 +104,50 @@ class ItssbeApplicationTests {
 
 		verify(equipmentRepo, times(1)).findAll();
 		verify(equipmentConverter, times(2)).toDTO(any(Equipment.class));
+	}
+
+	@Test
+	public void testGetEquipmentByIdWhenFound() {
+		// Arrange
+		int equipmentId = 1;
+
+		Equipment equipment = new Equipment();
+		equipment.setId(equipmentId);
+		equipment.setName("Equipment 1");
+
+		EquipmentDTO expectedDto = new EquipmentDTO();
+		expectedDto.setId(equipmentId);
+		expectedDto.setName("Equipment 1");
+
+		when(equipmentRepo.findById(equipmentId)).thenReturn(equipment);
+		when(equipmentConverter.toDTO(equipment)).thenReturn(expectedDto);
+
+		// Act
+		EquipmentDTO result = equipmentService.getEquipment(equipmentId);
+
+		// Assert
+		Assertions.assertEquals(expectedDto.getId(), result.getId());
+		Assertions.assertEquals(expectedDto.getName(), result.getName());
+
+		verify(equipmentRepo, times(1)).findById(equipmentId);
+		verify(equipmentConverter, times(1)).toDTO(equipment);
+	}
+
+	@Test
+	public void testGetEquipmentByIdWhenNotFound() {
+		// Arrange
+		int equipmentId = 1;
+
+		when(equipmentRepo.findById(equipmentId)).thenReturn(null);
+
+		// Act
+		EquipmentDTO result = equipmentService.getEquipment(equipmentId);
+
+		// Assert
+		Assertions.assertNull(result);
+
+		verify(equipmentRepo, times(1)).findById(equipmentId);
+		verifyNoMoreInteractions(equipmentConverter);
 	}
 
 }
