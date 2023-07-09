58fc17762c829e68f8f3bccb58f25537b31debe6
Nguyen Hoang Anh <hoanganh8d@gmail.com>
2023-07-09 09:22:50 +0700

    Add test function for getAllFeedBack

--- src/test/java/com/example/ITSSBE/ItssbeApplicationTests.java
+++ src/test/java/com/example/ITSSBE/ItssbeApplicationTests.java
@@ -1,14 +1,19 @@
 package com.example.ITSSBE;
 
 import com.example.ITSSBE.converter.EquipmentConverter;
+import com.example.ITSSBE.converter.FeedBackConverter;
 import com.example.ITSSBE.dto.EquipmentDTO;
+import com.example.ITSSBE.dto.FeedBackDTO;
 import com.example.ITSSBE.entity.EpCategory;
 import com.example.ITSSBE.entity.Equipment;
+import com.example.ITSSBE.entity.FeedBack;
 import com.example.ITSSBE.entity.Room;
 import com.example.ITSSBE.repository.IEpCategoryRepo;
 import com.example.ITSSBE.repository.IEquipmentRepo;
+import com.example.ITSSBE.repository.IFeedBackRepo;
 import com.example.ITSSBE.repository.IRoomRepo;
 import com.example.ITSSBE.service.EquipmentService;
+import com.example.ITSSBE.service.FeedBackService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,14 +44,21 @@ class ItssbeApplicationTests {
 	@Mock
 	private IEpCategoryRepo epCategoryRepo;
 
+	@Mock
+	private IFeedBackRepo feedBackRepo;
+
 	private EquipmentConverter equipmentConverter;
 	private EquipmentService equipmentService;
+	private FeedBackConverter feedBackConverter;
+	private FeedBackService feedBackService;
 
 	@BeforeEach
 	public void setup() {
 		MockitoAnnotations.openMocks(this);
 		equipmentConverter = new EquipmentConverter();
 		equipmentService = new EquipmentService();
+		feedBackConverter = new FeedBackConverter();
+		feedBackService = new FeedBackService();
 	}
 
 	@Test
@@ -252,6 +264,44 @@ class ItssbeApplicationTests {
 		verify(equipmentRepo, times(1)).findById(equipmentId);
 	}
 
+
+	@Test
+	public void testGetAllFeedBack() {
+		// Arrange
+		FeedBack feedBack1 = new FeedBack();
+		feedBack1.setId(1);
+		feedBack1.setContent("Feedback 1");
+
+		FeedBack feedBack2 = new FeedBack();
+		feedBack2.setId(2);
+		feedBack2.setContent("Feedback 2");
+
+		List<FeedBack> mockFeedBacks = Arrays.asList(feedBack1, feedBack2);
+
+		FeedBackDTO feedBackDTO1 = new FeedBackDTO();
+		feedBackDTO1.setId(1);
+		feedBackDTO1.setContent("Feedback 1");
+
+		FeedBackDTO feedBackDTO2 = new FeedBackDTO();
+		feedBackDTO2.setId(2);
+		feedBackDTO2.setContent("Feedback 2");
+
+		when(feedBackRepo.findAll()).thenReturn(mockFeedBacks);
+		when(feedBackConverter.toDTO(feedBack1)).thenReturn(feedBackDTO1);
+		when(feedBackConverter.toDTO(feedBack2)).thenReturn(feedBackDTO2);
+
+		// Act
+		List<FeedBackDTO> result = feedBackService.getAllFeedBack();
+
+		// Assert
+		Assertions.assertEquals(2, result.size());
+		Assertions.assertEquals(feedBackDTO1, result.get(0));
+		Assertions.assertEquals(feedBackDTO2, result.get(1));
+
+		verify(feedBackRepo, times(1)).findAll();
+		verify(feedBackConverter, times(1)).toDTO(feedBack1);
+		verify(feedBackConverter, times(1)).toDTO(feedBack2);
+	}
 
 
 }
