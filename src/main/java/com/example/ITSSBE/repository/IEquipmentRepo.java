ef6eb47fbb2ac8e8ec8a94d7e5663c35fe09dcff
phuctong2002 <vanphuc15092002@gmail.com>
2023-07-15 22:19:27 +0700

    fix tuan

--- src/main/java/com/example/ITSSBE/repository/IEquipmentRepo.java
+++ src/main/java/com/example/ITSSBE/repository/IEquipmentRepo.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IEquipmentRepo extends JpaRepository<Equipment, Integer> {
     @Query( value = "SELECT * FROM equipment WHERE room = :id ", nativeQuery = true)
-    Equipment findById(@Param("id") int id);
+    List<Equipment> findById(@Param("id") int id);
     @Query( value = "SELECT * FROM equipment WHERE name = :name ", nativeQuery = true)
     Equipment findByName(@Param("name") String name);
     @Query( value = "DELETE FROM equipment WHERE id = :id LIMIT 1", nativeQuery = true)
